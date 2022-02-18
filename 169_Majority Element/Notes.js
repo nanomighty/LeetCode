227c1d40c27d3bcf43d42c261a5f7068de9258ba
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-02-18 11:03:28 -0800

    Update Notes.js
    Used hash table

--- 169_Majority Element/Notes.js
+++ 169_Majority Element/Notes.js
@@ -1,13 +1,25 @@
-// Still incorrect
 
 var majorityElement = function(nums) {
-    let newArr = [];
-    for (let i = 0; i<nums.length; i++){
-      newArr = nums.filter(x => x == nums[i]);
-        if (newArr.length > (nums.length)/2){
-            return nums[i]
-        } else {
-            newArr = [];
+    let majority = 0;
+    let obj = {};
+    let value;
+    
+    for(let i = 0; i < nums.length; i++){
+        if (obj[nums[i]]){
+           obj[nums[i]]++
         }
+        else{
+            obj[nums[i]] = 1;
+        }
+        
+        if (obj[nums[i]] > majority){
+           majority = obj[nums[i]];
+            value = nums[i];
+        }
+        
     }
+    
+    return value;
+    
+    
 };
