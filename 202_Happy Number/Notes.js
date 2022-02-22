f515cc80a9fd544c05d025ccbc1badf3da210050
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-02-22 02:06:11 -0800

    Update Notes.js
    Another solution

--- 202_Happy Number/Notes.js
+++ 202_Happy Number/Notes.js
@@ -1,19 +1,24 @@
 
 var isHappy = function(n) {
-    if(n < 0 ){
-        return false;
-    }
     
-    let arr = n.toString().split('');
-    let sum = 0;
+    let seen = {};
     
-    if (sum = 0 || sum > 1){
-        for(let i=0; i<arr.length;i++){
-            sum += (arr[i])^2;
+    seen[n]=true;
+    
+    while (true) {
+           
+        let arr = n.toString().split('');
+        
+        
+        arr.map(function(digit){return parseInt(digit)})
+        .reduce(function(total, digit){return total + digit*digit}, 0)
+        
+        if(seen[n] === 1){
+            return true;
+        } else if (seen[n]){
+            return false;
+        } else {
+            seen[n] = true;
         }
-    }
-    
-    if (sum = 1){
-        return true;
-    }
+        }
 };
