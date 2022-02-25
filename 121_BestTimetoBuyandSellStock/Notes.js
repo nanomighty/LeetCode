dfa9692ef2cc6db485b86506b0b3ddd534c6f87e
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-02-25 02:09:08 -0800

    Update Notes.js

--- 121_BestTimetoBuyandSellStock/Notes.js
+++ 121_BestTimetoBuyandSellStock/Notes.js
@@ -1,31 +1,13 @@
 
 var maxProfit = function(prices) {
-    let minPrice = prices[0];
-    let maxPrice = 0;
+    let minPrice = prices[0]
     let profit = 0;
     
-    
-    for(let i=1; i<prices.length; i++){
-        if(prices[i]<minPrice){
-            minPrice = prices[i];
-        
-            for(let j = i+1; j<prices.length; j++){
-                if(prices[j] > maxPrice){
-                    maxPrice = prices[j];
-                }
-            } 
-            
-            profit = maxPrice - minPrice;
-            
-        }    
+    for (let i = 0; i<prices.length; i++){
+        minPrice = Math.min(minPrice, prices[i]);
+        profit = Math.max(profit, prices[i] - minPrice )
     }
     
-    
-    
-    if (profit < 0){
-        return 0;
-    } else {
-        return profit;
-    }
+    return profit;
     
 };
