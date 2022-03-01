f4cd470d3f85c5682e304d0d4488359c83c3dd08
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-02-28 23:38:21 -0800

    Update Notes.js

--- 2186_MinimumNumberofStepstoMakeTwoStringsAnagramII/Notes.js
+++ 2186_MinimumNumberofStepstoMakeTwoStringsAnagramII/Notes.js
@@ -1,40 +1,40 @@
-
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
 var minSteps = function(s, t) {
-    let tracking = 0;
+    let sObj = {};
+    let tObj = {};
     
-    
-    for(let i = 0; i<s.length; i++){
-        if(t.indexOf(s[i]) == -1){*/
-var minSteps = function(s, t) {
-    let tracking = 0;
-    
-    
-    for(let i = 0; i<s.length; i++){
-        if(t.indexOf(s[i]) == -1){
-            tracking++;
-        }
+    for(let char of s){
+        sObj[char] = sObj[char] + 1 || 1
     }
     
-    for(let j=0; j<t.length; j++){
-        if(s.indexOf(t[j]) == -1){
-            tracking++;
-        }
+    for(let char of t){
+        if(sObj[char]) = sObj[char] - 1 || 1
     }
     
-    return tracking;
+    for(let char of t){
+        tObj[char] = tObj[char] + 1 || 1
+    }
+    
+    for(let char of s){
+        tObj[char] = tObj[char] - 1 || 1
+    }
+    
+    let sArray = Object.values(sObj);
+    let tArray = Object.values(tObj);
+    
+    let sum = 0;
+    
+    for(let val of sArray){
+        sum += val;
+    }
+    
+    for(let val of tArray){
+        sum += val;
+    }
+    
+    return sum;
 };
-
-
-            tracking++;
-        }
-    }
-    
-    for(let j=0; j<t.length; j++){
-        if(s.indexOf(t[j]) == -1){
-            tracking++;
-        }
-    }
-    
-    return tracking;
-};
-
