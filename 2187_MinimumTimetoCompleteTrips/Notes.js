590d675472e5f5c1a4e3fde34a2ca790bdc30edb
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-03-02 18:37:17 -0800

    Second attempt

--- 2187_MinimumTimetoCompleteTrips/Notes.js
+++ 2187_MinimumTimetoCompleteTrips/Notes.js
@@ -1,27 +1,23 @@
 
 var minimumTime = function(time, totalTrips) {
+  let low = 1;
+  let high = 100000000000000 + 1;
     
-    let tripCount = 0;
-        
-    let timeCount = 1;
-    while(tripCount < totalTrips){
-        
+    while (low < high){
+          let mid = Math.floor((low+high)/2);
+    
+          if(isEnough(mid)){
+              high = mid;
+          } else{
+              low = mid + 1;
+          }
+    } return low;
+    
+    function isEnough(midTime){
+        let trips = 0;
         for(let i = 0; i<time.length; i++){
-            if(timeCount >= time[i]){
-                tripCount += Math.floor(timeCount/time[i]);
-            }
+            trips += Math.floor(midTime/time[i]);  
         }
-        
-        if(tripCount < totalTrips){
-            tripCount = 0;
-        }
-        
-        timeCount++  
+        return trips >= totalTrips;
     }
-    
-    timeCount = timeCount - 1;
-    
-    return timeCount;
 };
-
-
