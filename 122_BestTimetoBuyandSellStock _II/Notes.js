c3ac1dfd1c296955308cef1832720dbbd6a32700
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-03-10 23:47:37 -0800

    Initial attempt

--- 122_BestTimetoBuyandSellStock _II/Notes.js
+++ 122_BestTimetoBuyandSellStock _II/Notes.js
@@ -1,1 +1,17 @@
-//Enter code here
+
+
+var maxProfit = function(prices) {
+    
+    let profit = 0;
+    let buy = prices[0];
+    let sell = prices[1];
+    
+    
+    for(let i=0; i<prices.length; i++){
+        buy = Math.min(buy, prices[i]);
+        sell = Math.max(sell, prices[i]);
+        profit = sell - buy;
+    }
+    
+    return profit;
+};
