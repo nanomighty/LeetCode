fd0de2e2a21353047c5a15790ff7776ccb0d7ee8
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-03-10 23:55:42 -0800

    Second attempt

--- 122_BestTimetoBuyandSellStock _II/Notes.js
+++ 122_BestTimetoBuyandSellStock _II/Notes.js
@@ -3,15 +3,12 @@
 var maxProfit = function(prices) {
     
     let profit = 0;
-    let buy = prices[0];
-    let sell = prices[1];
     
-    
-    for(let i=0; i<prices.length; i++){
-        buy = Math.min(buy, prices[i]);
-        sell = Math.max(sell, prices[i]);
-        profit = sell - buy;
+    for(let i = 0; i<prices.length-1; i++){
+        if(prices[i+1]>prices[i]){
+            profit += prices[i+1]-prices[i];
+        }
     }
-    
     return profit;
+    
 };
