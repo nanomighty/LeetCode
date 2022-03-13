0aa802018ea5800811a802e60a4280cbef887662
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-03-12 19:39:21 -0800

    Updated notes

--- 6031_Find All K-Distant Indices in an Array/Notes.js
+++ 6031_Find All K-Distant Indices in an Array/Notes.js
@@ -1,1 +1,24 @@
-
+var findKDistantIndices = function(nums, key, k) {
+  let kDistantArr = [];
+  let indices = [];
+  let idx = nums.indexOf(key);
+  
+  while(idx != -1){
+    indices.push(idx);
+    idx = nums.indexOf(key, idx + 1);
+  }
+  
+  for(let i=0; i<nums.length; i++){
+    for(let j=0; j<indices.length; j++){
+      if(Math.abs(i - indices[j]) <= k){
+        kDistantArr.push(i);
+      }
+    }
+  }
+  
+  kDistantArr = [... new Set(kDistantArr)];
+  
+  return kDistantArr;
+  
+  
+}
