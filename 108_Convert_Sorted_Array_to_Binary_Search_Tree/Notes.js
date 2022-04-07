9a74f177265cd4771e4d03ef7efb429d3021b5df
Nancy Lewis <12788346+nanomighty@users.noreply.github.com>
2022-04-06 23:29:05 -0700

    Create Notes.js

---
+++ 108_Convert_Sorted_Array_to_Binary_Search_Tree/Notes.js
@@ -0,0 +1,18 @@
+// **parameters**
+// @param {numbers[]} nums
+// @return {TreeNode}
+
+//  **Definition for a binary tree node.**
+//  function TreeNode(val, left, right) {
+//       this.val = (val===undefined ? 0 : val)
+//       this.left = (left===undefined ? null : left)
+//       this.right = (right===undefined ? null : right)
+//   }
+  
+ var sortedArrayToBST = function(nums){
+  
+   
+   
+ 
+ }
+ 
